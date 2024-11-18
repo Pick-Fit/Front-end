023f0dafebc9cfe4a30a7c2d1a6c2916feb4f4a3
5hyunseong <dhgustjd96@gmail.com>
2024-11-18 10:48:33 +0900

    feat mainPage

--- pickfit/src/App.js
+++ pickfit/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext"; // AuthProvider 임포트
+import Home from "./Home";
+import LoginPage from "./LoginPage";
+import MyPage from "./MyPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
